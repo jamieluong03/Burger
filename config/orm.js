fe78a2133fc085ed2fb93f160b3feecf73389402
Jamie Luong <jamieluong03@gmail.com>
2019-10-26 11:17:05 -0700

    added and updated js files

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// import MySQL connection
+var connection = require("../config/connection.js");
+
+// get function
+function selectAll();
+
+
+// post function
+function insertOne();
+
+
+// update function
+function updateOne();
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("McDouble", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", false);
+

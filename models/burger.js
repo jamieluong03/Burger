340b4701ca6b2ca1e92d5fcbcd23d97140cf3e6f
Jamie Luong <jamieluong03@gmail.com>
2019-10-26 11:31:48 -0700

    updated orm and model

--- models/burger.js
+++ models/burger.js
@@ -3,19 +3,19 @@ var orm = require("../config/orm.js");
 
 // create code that will fall the ORM functions using burger specific input for the ORM
 var burger = {
-    all: function(cb) {
+    selectAll: function(cb) {
         orm.all("burgers", function(res) {
             cb(res);
         });
     },
 
-    create: function(cols, vals, cb){
+    insertOne: function(cols, vals, cb){
         orm.create("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
 
-    update: function(objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
         orm.update("burgers", objColVals, condition, function(res) {
             cb(res);
         });

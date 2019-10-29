771447aeb38ed037e7fafd084618d87ec33f92e8
Jamie Luong <jamieluong03@gmail.com>
2019-10-29 00:17:07 -0700

    heroku commit

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Hellojamie3!",
-  database: "burgers_db"
+  username: "xnoiy35xgo7zx249",
+  password: "dhyk20hh51afdcie",
+  database: "ctgdkav6510fscqo"
 });
 
 // Make connection.

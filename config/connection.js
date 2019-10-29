3444a6a7bb41277fc5a691ad3abfa8d11740a7e2
Jamie Luong <jamieluong03@gmail.com>
2019-10-29 10:18:25 -0700

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  username: "xnoiy35xgo7zx249",
-  password: "dhyk20hh51afdcie",
-  database: "ctgdkav6510fscqo"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Hellojamie3!",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
